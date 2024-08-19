f252accec40beb427797c7cd0131cf28c7f3b8fd
tushar-sadat <tusharsadat30@gmail.com>
2024-08-19 17:11:53 +0600

    Render Tasks

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class TaskResource extends JsonResource
 {
@@ -14,6 +16,22 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
+            'due_date' => (new Carbon($this->due_date))->format('Y-m-d'),
+            'status' => $this->status,
+            'priority' => $this->priority,
+            'image_path' => $this->image_path && !(str_starts_with($this->image_path, 'http')) ?
+                Storage::url($this->image_path) : $this->image_path,
+            'project_id' => $this->project_id,
+            'project' => new ProjectResource($this->project),
+            'assigned_user_id' => $this->assigned_user_id,
+            'assignedUser' => $this->assignedUser ? new UserResource($this->assignedUser) : null,
+            'createdBy' => new UserResource($this->createdBy),
+            'updatedBy' => new UserResource($this->updatedBy),
+        ];
     }
 }
---
+++ resources/js/Pages/Task/Index.jsx
@@ -0,0 +1,30 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import React from "react";
+import TasksTable from "./TasksTable";
+
+const Index = ({ auth, tasks, queryParams = null }) => {
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+          Task
+        </h2>
+      }
+    >
+      <Head title="tasks" />
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900 dark:text-gray-100">
+              <TasksTable tasks={tasks} queryParams={queryParams}></TasksTable>
+            </div>
+          </div>
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  );
+};
+
+export default Index;
